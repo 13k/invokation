36f8a029102b7946ec3f421430dc85ebbbf0e5c6
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2019-07-11 11:39:49 -0300

    Add more debugging commands

--- game/scripts/vscripts/game_mode/commands.lua
+++ game/scripts/vscripts/game_mode/commands.lua
@@ -3,123 +3,122 @@ local Logger = require("lib.logger")
 local Invoker = require("dota2.invoker")
 
 function GameMode:registerCommands()
-  --[[
-  Convars:RegisterCommand(
-    "events_test",
-    function()
-      self:StartEventTest()
-    end,
-    "events test",
-    0
-  )
-  ]]
-  --[[
-  Convars:RegisterCommand(
-    "command_example",
-    lfunc.bindbyname(GameMode, "ExampleConsoleCommand"),
-    "A console command example",
-    FCVAR_CHEAT
-  )
-  ]]
   Convars:RegisterCommand(
     "inv_debug",
-    lfunc.bindbyname(self, "enableDebug"),
-    "Enable Invokation debugging",
+    lfunc.bindbyname(self, "setDebug"),
+    "Set Invokation debugging (empty - print debug status, 0 - disabled, 1 - enabled)",
     FCVAR_CHEAT
   )
 
   Convars:RegisterCommand(
-    "inv_debug_off",
-    lfunc.bindbyname(self, "disableDebug"),
-    "Disable Invokation debugging",
+    "inv_dump_abilities",
+    lfunc.bindbyname(self, "dumpAbilities"),
+    "Dump current hero abilities (empty - verbose, 1 - simplified)",
     FCVAR_CHEAT
   )
 
   Convars:RegisterCommand(
-    "inv_debug_abilities",
-    lfunc.bindbyname(self, "debugAbilities"),
-    "Print debug information for current hero abilities",
-    FCVAR_CHEAT
-  )
-
-  Convars:RegisterCommand(
-    "inv_swap_abilities",
-    lfunc.bindbyname(self, "swapAbilities"),
-    "Swaps two abilities (given by name), disabling the one that was swapped out",
-    FCVAR_CHEAT
-  )
-
-  Convars:RegisterCommand(
-    "inv_invoke_ability",
+    "inv_invoke",
     lfunc.bindbyname(self, "invokeAbility"),
-    "Invoke an ability (given by name)",
+    "Invoke an ability (<name:string>)",
     FCVAR_CHEAT
   )
 
   Convars:RegisterCommand(
-    "inv_combo_graph_dump",
+    "inv_dump_combo_graph",
     lfunc.bindbyname(self, "dumpComboGraph"),
     "Dumps a combo's finite state machine in DOT format",
+    FCVAR_CHEAT
+  )
+
+  Convars:RegisterCommand(
+    "inv_music_status",
+    lfunc.bindbyname(self, "changeMusicStatus"),
+    "Change music status (<status:int> <intensity:float>)",
+    FCVAR_CHEAT
+  )
+
+  Convars:RegisterCommand(
+    "inv_dump_lua_version",
+    lfunc.bindbyname(self, "dumpLuaVersion"),
+    "Dump Lua version",
+    FCVAR_CHEAT
+  )
+
+  Convars:RegisterCommand(
+    "inv_dump_global",
+    lfunc.bindbyname(self, "dumpGlobal"),
+    "Dump global value (<name:string>)",
+    FCVAR_CHEAT
+  )
+
+  Convars:RegisterCommand(
+    "inv_find_global",
+    lfunc.bindbyname(self, "findGlobal"),
+    "Find global name (<pattern:regex>)",
     FCVAR_CHEAT
   )
 
   self:d("  register commands")
 end
 
-function GameMode:enableDebug()
-  self.logger.level = Logger.DEBUG
+function GameMode:setDebug(_, arg)
+  if arg == "1" then
+    self.logger.level = Logger.DEBUG
+  elseif arg == "0" then
+    self.logger.level = Logger.INFO
+  else
+    print(string.format("inv_debug = %d", self.logger.level <= Logger.DEBUG and 1 or 0))
+  end
 end
 
-function GameMode:disableDebug()
-  self.logger.level = Logger.INFO
+local function debugAbility(a, simple)
+  if a == nil then
+    return nil
+  end
+
+  if simple then
+    return {
+      a:GetAbilityIndex(),
+      a:GetAbilityName(),
+      a:GetLevel(),
+    }
+  end
+
+  return {
+    name = a:GetAbilityName(),
+    index = a:GetAbilityIndex(),
+    type = a:GetAbilityType(),
+    level = a:GetLevel(),
+    maxLevel = a:GetMaxLevel(),
+    damage = a:GetAbilityDamage(),
+    damageType = a:GetAbilityDamageType(),
+    isCastable = a:IsFullyCastable(),
+    isActivated = a:IsActivated(),
+    isHidden = a:IsHidden(),
+    isAttributeBonus = a:IsAttributeBonus(),
+    isItem = a:IsItem(),
+    isPassive = a:IsPassive(),
+    procsMagicStick = a:ProcsMagicStick(),
+    isTrained = a:IsTrained(),
+    canBeUpgraded = a:CanAbilityBeUpgraded(),
+    heroLevelToUpgrade = a:GetHeroLevelRequiredToUpgrade(),
+    duration = a:GetDuration(),
+    castRange = a:GetCastRange(),
+    castPoint = a:GetCastPoint(),
+    backswingTime = a:GetBackswingTime(),
+    channelTime = a:GetChannelTime(),
+  }
 end
 
-function GameMode:debugAbilities(...)
+function GameMode:dumpAbilities(_, simple)
   local player = Convars:GetDOTACommandClient()
   local hero = player:GetAssignedHero()
 
   for i = 0, hero:GetAbilityCount() - 1 do
     local a = hero:GetAbilityByIndex(i)
-    local t = nil
-
-    if a ~= nil then
-      t = {
-        name = a:GetAbilityName(),
-        index = a:GetAbilityIndex(),
-        type = a:GetAbilityType(),
-        level = a:GetLevel(),
-        maxLevel = a:GetMaxLevel(),
-        damage = a:GetAbilityDamage(),
-        damageType = a:GetAbilityDamageType(),
-        isCastable = a:IsFullyCastable(),
-        isActivated = a:IsActivated(),
-        isHidden = a:IsHidden(),
-        isAttributeBonus = a:IsAttributeBonus(),
-        isItem = a:IsItem(),
-        isPassive = a:IsPassive(),
-        procsMagicStick = a:ProcsMagicStick(),
-        isTrained = a:IsTrained(),
-        canBeUpgraded = a:CanAbilityBeUpgraded(),
-        heroLevelToUpgrade = a:GetHeroLevelRequiredToUpgrade(),
-        duration = a:GetDuration(),
-        castRange = a:GetCastRange(),
-        castPoint = a:GetCastPoint(),
-        backswingTime = a:GetBackswingTime(),
-        channelTime = a:GetChannelTime(),
-      }
-    end
-
-    self:d(i, t)
+    self:d(i, debugAbility(a, simple))
   end
-end
-
-function GameMode:swapAbilities(_, left, right)
-  self:d("swapAbilities()", left, right)
-
-  local player = Convars:GetDOTACommandClient()
-  local hero = player:GetAssignedHero()
-
-  hero:SwapAbilities(left, right, true, true)
 end
 
 function GameMode:invokeAbility(_, ability)
@@ -133,8 +132,6 @@ function GameMode:invokeAbility(_, ability)
 end
 
 function GameMode:dumpComboGraph(_, comboName)
-  self:d("dumpComboGraph()", comboName)
-
   local combo = self.combos:Find(comboName)
 
   if combo == nil then
@@ -143,4 +140,56 @@ function GameMode:dumpComboGraph(_, comboName)
   end
 
   print(combo:todot())
+end
+
+function GameMode:changeMusicStatus(_, status, intensity)
+  local player = Convars:GetDOTACommandClient()
+
+  status = tonumber(status)
+  intensity = tonumber(intensity)
+
+  player:SetMusicStatus(status, intensity)
+end
+
+function GameMode:dumpLuaVersion()
+  print(_VERSION)
+end
+
+function GameMode:dumpGlobal(_, name)
+  local value = _G
+
+  for segment in name:gmatch("([^.]+)%.?") do
+    value = value[segment]
+  end
+
+  local pp = require("pl.pretty")
+  local typ = type(value)
+  local repr = pp.write(value)
+
+  print(string.format("%q (%s): %s", name, typ, repr))
+
+  if typ == "function" then
+    local info = debug.getinfo(value)
+    print(string.format("source: %s:%d", info.source, info.linedefined))
+  end
+end
+
+function GameMode:findGlobal(_, pattern)
+  local matches = {}
+
+  for name, _ in pairs(_G) do
+    if name:match(pattern) then
+      table.insert(matches, name)
+    end
+  end
+
+  table.sort(matches)
+
+  print(string.format("Globals matching pattern %q:", pattern))
+
+  for _, match in ipairs(matches) do
+    print(string.format(" * %s", match))
+  end
+
+  print("---")
 end
