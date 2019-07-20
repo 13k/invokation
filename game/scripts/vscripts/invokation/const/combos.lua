9d1d943f2b565332bcbf0b54c311dabf8ae86ea2
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2019-07-20 04:50:44 -0300

    [wip] Fix and add more combos

--- game/scripts/vscripts/invokation/const/combos.lua
+++ game/scripts/vscripts/invokation/const/combos.lua
@@ -7,7 +7,8 @@
 
 local M = {}
 
---- Laning Phase
+-------------------------------------------------------------------------------
+-- Laning Phase
 -- @section laning_phase
 
 --- @table invokation_combo_lane_anti_gank1
@@ -138,7 +139,7 @@ M.invokation_combo_lane_harass_forge_spirit2 = {
   specialty = "qe",
   category = "laning_phase",
   stance = "offensive",
-  heroLevel = 2,
+  heroLevel = 3,
   damageRating = 1,
   difficultyRating = 1,
   items = {"item_null_talisman"},
@@ -274,7 +275,7 @@ M.invokation_combo_solo_control_magic_damage5 = {
   heroLevel = 6,
   damageRating = 2,
   difficultyRating = 3,
-  items = {"item_null_talisman", "item_null_talisman"},
+  items = {"item_null_talisman", "item_null_talisman", "item_null_talisman"},
   sequence = {
     [1] = {
       name = "invoker_ice_wall",
@@ -374,7 +375,8 @@ M.invokation_combo_solo_control_physical_damage1 = {
   },
 }
 
---- Teamfight
+-------------------------------------------------------------------------------
+-- Teamfight
 -- @section teamfight
 
 --- @table invokation_combo_tf_control_magic_damage1
@@ -393,12 +395,12 @@ M.invokation_combo_tf_control_magic_damage1 = {
       next = {2},
     },
     [2] = {
-      name = "invoker_cold_snap",
+      name = "invoker_ice_wall",
       required = true,
       next = {3},
     },
     [3] = {
-      name = "invoker_ice_wall",
+      name = "invoker_cold_snap",
       required = true,
       next = {4},
     },
@@ -409,7 +411,119 @@ M.invokation_combo_tf_control_magic_damage1 = {
   },
 }
 
---- Late Game
+--- @table invokation_combo_tf_control_magic_damage2
+M.invokation_combo_tf_control_magic_damage2 = {
+  specialty = "qe",
+  category = "teamfight",
+  stance = "offensive",
+  heroLevel = 12,
+  damageRating = 2,
+  difficultyRating = 3,
+  items = {"item_null_talisman", "item_null_talisman", "item_ultimate_scepter"},
+  sequence = {
+    [1] = {
+      name = "invoker_tornado",
+      required = true,
+      next = {2},
+    },
+    [2] = {
+      name = "invoker_chaos_meteor",
+      required = true,
+      next = {3},
+    },
+    [3] = {
+      name = "invoker_deafening_blast",
+      required = true,
+      next = {4},
+    },
+    [4] = {
+      name = "invoker_cold_snap",
+      required = true,
+      next = {5},
+    },
+    [5] = {
+      name = "invoker_ice_wall",
+      required = true,
+    },
+  },
+}
+
+--- @table invokation_combo_tf_control_magic_damage3
+M.invokation_combo_tf_control_magic_damage3 = {
+  specialty = "qe",
+  category = "teamfight",
+  stance = "offensive",
+  heroLevel = 12,
+  damageRating = 3,
+  difficultyRating = 3,
+  items = {"item_null_talisman", "item_null_talisman", "item_ultimate_scepter", "item_cyclone"},
+  sequence = {
+    [1] = {
+      name = "item_cyclone",
+      required = true,
+      next = {2},
+    },
+    [2] = {
+      name = "invoker_sun_strike",
+      required = true,
+      next = {3},
+    },
+    [3] = {
+      name = "invoker_emp",
+      required = true,
+      next = {4},
+    },
+    [4] = {
+      name = "invoker_chaos_meteor",
+      required = true,
+      next = {5},
+    },
+    [5] = {
+      name = "invoker_deafening_blast",
+      required = true,
+    },
+  },
+}
+
+--- @table invokation_combo_tf_control_physical_damage1
+M.invokation_combo_tf_control_physical_damage1 = {
+  specialty = "qe",
+  category = "teamfight",
+  stance = "offensive",
+  heroLevel = 12,
+  damageRating = 2,
+  difficultyRating = 3,
+  items = {"item_null_talisman", "item_null_talisman", "item_ultimate_scepter"},
+  sequence = {
+    [1] = {
+      name = "invoker_forge_spirit",
+      required = true,
+      next = {2},
+    },
+    [2] = {
+      name = "invoker_alacrity",
+      required = true,
+      next = {3},
+    },
+    [3] = {
+      name = "invoker_ice_wall",
+      required = true,
+      next = {4},
+    },
+    [4] = {
+      name = "invoker_cold_snap",
+      required = true,
+      next = {5},
+    },
+    [5] = {
+      name = "invoker_sun_strike",
+      required = true,
+    },
+  },
+}
+
+-------------------------------------------------------------------------------
+-- Late Game
 -- @section late_game
 
 --- @table invokation_combo_late_control_magic_damage1
@@ -460,6 +574,144 @@ M.invokation_combo_late_control_magic_damage1 = {
     },
     [7] = {
       name = "invoker_sun_strike",
+      required = true,
+    },
+  },
+}
+
+--- @table invokation_combo_late_control_magic_damage2
+M.invokation_combo_late_control_magic_damage2 = {
+  specialty = "qe",
+  category = "late_game",
+  stance = "offensive",
+  damageRating = 4,
+  difficultyRating = 4,
+  heroLevel = 18,
+  items = {
+    "item_null_talisman",
+    "item_null_talisman",
+    "item_null_talisman",
+    "item_ultimate_scepter",
+    "item_octarine_core",
+    "item_refresher",
+  },
+  sequence = {
+    [1] = {
+      name = "invoker_tornado",
+      required = true,
+      next = {2},
+    },
+    [2] = {
+      name = "invoker_emp",
+      required = true,
+      next = {3},
+    },
+    [3] = {
+      name = "invoker_sun_strike",
+      required = true,
+      next = {4},
+    },
+    [4] = {
+      name = "invoker_chaos_meteor",
+      required = true,
+      next = {5},
+    },
+    [5] = {
+      name = "invoker_deafening_blast",
+      required = true,
+      next = {6},
+    },
+    [6] = {
+      name = "item_refresher",
+      required = true,
+      next = {7},
+    },
+    [7] = {
+      name = "invoker_chaos_meteor",
+      required = true,
+      next = {8},
+    },
+    [8] = {
+      name = "invoker_deafening_blast",
+      required = true,
+      next = {9},
+    },
+    [9] = {
+      name = "invoker_sun_strike",
+      required = true,
+    },
+  },
+}
+
+--- @table invokation_combo_late_control_magic_damage3
+M.invokation_combo_late_control_magic_damage3 = {
+  specialty = "qe",
+  category = "late_game",
+  stance = "offensive",
+  damageRating = 4,
+  difficultyRating = 4,
+  heroLevel = 18,
+  items = {
+    "item_null_talisman",
+    "item_null_talisman",
+    "item_refresher",
+    "item_ultimate_scepter",
+    "item_sheepstick",
+    "item_black_king_bar",
+  },
+  sequence = {
+    [1] = {
+      name = "item_sheepstick",
+      required = true,
+      next = {2},
+    },
+    [2] = {
+      name = "invoker_tornado",
+      required = true,
+      next = {3},
+    },
+    [3] = {
+      name = "item_black_king_bar",
+      required = true,
+      next = {4},
+    },
+    [4] = {
+      name = "invoker_chaos_meteor",
+      required = true,
+      next = {5},
+    },
+    [5] = {
+      name = "invoker_deafening_blast",
+      required = true,
+      next = {6},
+    },
+    [6] = {
+      name = "item_refresher",
+      required = true,
+      next = {7},
+    },
+    [7] = {
+      name = "invoker_chaos_meteor",
+      required = true,
+      next = {8},
+    },
+    [8] = {
+      name = "invoker_deafening_blast",
+      required = true,
+      next = {9},
+    },
+    [9] = {
+      name = "invoker_sun_strike",
+      required = true,
+      next = {10},
+    },
+    [10] = {
+      name = "item_black_king_bar",
+      required = true,
+      next = {11},
+    },
+    [11] = {
+      name = "item_sheepstick",
       required = true,
     },
   },
