059f1f9c05716d32e30e471a6b775f7d8766ba12
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2019-08-14 01:27:59 -0300

    Refactor `PickerCombo` component

--- content/panorama/scripts/custom_game/picker_combo.js
+++ content/panorama/scripts/custom_game/picker_combo.js
@@ -1,8 +1,25 @@
 "use strict";
 
 (function(global, context) {
+  var _ = global.lodash;
+  var ParallelSequence = global.Sequence.ParallelSequence;
   var Sequence = global.Sequence.Sequence;
   var CreateComponent = context.CreateComponent;
+
+  function propertyCssClass(property, value) {
+    var baseClass;
+
+    switch (property) {
+      case "damageRating":
+      case "difficultyRating":
+        baseClass = "rating";
+        break;
+      default:
+        baseClass = _.snakeCase(property);
+    }
+
+    return baseClass + "_" + String(value);
+  }
 
   var PickerCombo = CreateComponent({
     constructor: function PickerCombo() {
@@ -10,8 +27,8 @@
         elements: {
           titleLabel: "Title",
           heroLevelLabel: "HeroLevelLabel",
-          damageRatingButton: "DamageRating",
-          difficultyRatingButton: "DifficultyRating",
+          damageRating: "DamageRating",
+          difficultyRating: "DifficultyRating",
         },
         inputs: {
           SetCombo: "setCombo",
@@ -21,24 +38,52 @@
 
     setCombo: function(combo) {
       this.combo = combo;
+      this.render();
+    },
 
-      var specialtyClass = "specialty_" + combo.specialty;
-      var stanceClass = "stance_" + combo.stance;
-      var damageClass = "rating_" + combo.damageRating.toString();
-      var difficultyClass = "rating_" + combo.difficultyRating.toString();
+    // --- Actions ---
 
-      return new Sequence()
-        .SetDialogVariable(this.$ctx, "specialty", combo.l10n.specialty)
-        .SetDialogVariable(this.$ctx, "stance", combo.l10n.stance)
-        .SetDialogVariable(this.$ctx, "damage_rating", combo.l10n.damageRating)
-        .SetDialogVariable(this.$ctx, "difficulty_rating", combo.l10n.difficultyRating)
-        .SetAttribute(this.$titleLabel, "text", combo.l10n.name)
-        .SetAttribute(this.$heroLevelLabel, "text", combo.heroLevel)
+    setVariablesAction: function() {
+      return new ParallelSequence()
+        .SetDialogVariable(this.$ctx, "hero_level", this.combo.heroLevel)
+        .SetDialogVariable(this.$ctx, "specialty", this.combo.l10n.specialty)
+        .SetDialogVariable(this.$ctx, "stance", this.combo.l10n.stance)
+        .SetDialogVariable(this.$ctx, "damage_rating", this.combo.l10n.damageRating)
+        .SetDialogVariable(this.$ctx, "difficulty_rating", this.combo.l10n.difficultyRating);
+    },
+
+    setAttributesAction: function() {
+      return new ParallelSequence()
+        .SetAttribute(this.$titleLabel, "text", this.combo.l10n.name)
+        .SetAttribute(this.$heroLevelLabel, "text", String(this.combo.heroLevel));
+    },
+
+    setClassesAction: function() {
+      var specialtyClass = propertyCssClass("specialty", this.combo.specialty);
+      var stanceClass = propertyCssClass("stance", this.combo.stance);
+      var damageRatingClass = propertyCssClass("damageRating", this.combo.damageRating);
+      var difficultyRatingClass = propertyCssClass("difficultyRating", this.combo.difficultyRating);
+
+      return new ParallelSequence()
         .AddClass(this.$ctx, specialtyClass)
         .AddClass(this.$ctx, stanceClass)
-        .AddClass(this.$damageRatingButton, damageClass)
-        .AddClass(this.$difficultyRatingButton, difficultyClass)
-        .Start();
+        .AddClass(this.$damageRating, damageRatingClass)
+        .AddClass(this.$difficultyRating, difficultyRatingClass);
+    },
+
+    // ----- Action Runners -----
+
+    render: function() {
+      var seq = new Sequence()
+        .Action(this.setVariablesAction())
+        .Action(this.setAttributesAction())
+        .Action(this.setClassesAction());
+
+      this.debugFn(function() {
+        return ["render()", { id: this.combo.id, actions: seq.size() }];
+      });
+
+      return seq.Start();
     },
 
     ShowDetails: function() {
