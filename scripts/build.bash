cfd47015e11edf0dfd462baebf45a7f758fc8219
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2019-09-28 13:12:18 -0300

    Fix build task

--- scripts/build.bash
+++ scripts/build.bash
@@ -17,17 +17,37 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 # shellcheck source="./common.bash"
 source "$SCRIPT_DIR/common.bash"
 
-opts=(
-  "-i" "$ADDON_CONTENT_REL_PATH/*"
-  "-r"
-)
+compile() {
+  local args=("$@")
 
-if [[ -n "$FORCE_BUILD" ]]; then
-  opts=("${opts[@]}" "-fshallow")
-fi
+  if [[ -n "$FORCE_BUILD" ]]; then
+    args=("${args[@]}" "-fshallow")
+  fi
 
-cmd=("$RSRCC_BIN_PATH" "${opts[@]}")
+  local cmd=("$RSRCC_BIN_PATH" "${args[@]}")
 
-echo "> ${cmd[*]}"
-cd "$DOTA2_PATH"
-exec "${cmd[@]}"
+  pushd "$DOTA2_PATH" >/dev/null &&
+    echo "> ${cmd[*]}" &&
+    "${cmd[@]}" &&
+    popd >/dev/null
+}
+
+# Compile maps individually.
+# Recursive compilation including maps seems to break the compiler.
+
+SRC_MAPS_PATH="$SRC_CONTENT_PATH/maps"
+ADDON_MAPS_REL_PATH="$ADDON_CONTENT_REL_PATH/maps"
+
+readarray -t maps < <(find "$SRC_MAPS_PATH" -name '*.vmap' -printf '%P\n')
+
+for map in "${maps[@]}"; do
+  compile -i "$ADDON_MAPS_REL_PATH/$map" || exit $?
+done
+
+# Compile all other resources.
+
+filelist="$(mktemp)"
+
+find "$SRC_CONTENT_PATH" -path "$SRC_MAPS_PATH" -prune -o -type f -printf "$ADDON_CONTENT_REL_PATH/%P\\n" >"$filelist"
+
+compile -filelist "$(wslpath -w "$filelist")"
