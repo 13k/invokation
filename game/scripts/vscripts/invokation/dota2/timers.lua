8df1ef5fe22db7b01ddd33a6b66fed86e600e4e8
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2019-10-01 22:52:07 -0300

    [vscript] Fix `Timers`

--- game/scripts/vscripts/invokation/dota2/timers.lua
+++ game/scripts/vscripts/invokation/dota2/timers.lua
@@ -13,6 +13,10 @@ local M = {_VERSION = "1.06"}
 local THINK_NAME = "timers"
 local THINK_INTERVAL = 0.01
 
+local ERRF_INVALID_TIMER_ID = "Invalid timer id: %s [%s]"
+local ERRF_INVALID_TIMER_CALLBACK = "Invalid timer %q: callback must be a function"
+local ERRF_INVALID_TIMER_ALREADY_REGISTERED = "Invalid timer %q: already registered"
+
 --- Default error handler.
 -- @tparam string msg Error message
 -- @treturn string Error message and stack trace.
@@ -20,38 +24,32 @@ function M.defaultErrorHandler(msg)
   return msg .. "\n" .. debug.traceback() .. "\n"
 end
 
-local function generateUniqueName()
+local function generateUniqueId()
   return UniqueString("timer")
 end
 
 local function handleError(timer, err)
-  print("[ERROR] Timer " .. timer.name .. ":")
+  print("[ERROR] Timer " .. timer.id .. ":")
   print(err)
 end
 
 local Timer = require("pl.class")()
 
 function Timer:_init(options)
-  self.name = options.name or generateUniqueName()
+  self.id = options.id or generateUniqueId()
   self.callback = options.callback
-  self.context = options.context
+  self.args = options.args or {}
   self.realtime = options.realtime
   self.delay = options.delay or 0
   self.onError = options.onError or M.defaultErrorHandler
   self.paused = options.paused
+  self.at = (options.at or self:now()) + self.delay
 
-  assert(self.callback, string.format("Invalid timer %q: no callback provided", self.name))
+  assert(type(self.id) == "string", ERRF_INVALID_TIMER_ID:format(self.id, type(self.id)))
+  assert(type(self.callback) == "function", ERRF_INVALID_TIMER_CALLBACK:format(self.id))
 
-  self.at = self:now() + self.delay
-
-  if self.context then
-    self.callbackWrapper = function()
-      return self.callback(self.context, self)
-    end
-  else
-    self.callbackWrapper = function()
-      return self.callback(self)
-    end
+  self.callbackWrapper = function()
+    return self.callback(unpack(self.args))
   end
 end
 
@@ -63,10 +61,12 @@ end
 --
 -- Should be placed inside the `Activate()` function.
 --
-function M:Start()
+-- @tparam[opt=0.01] float interval Timer processing interval
+function M:Start(interval)
+  self.interval = interval or THINK_INTERVAL
   self.timers = {}
-  -- local ent = SpawnEntityFromTableSynchronous("info_target", {targetname="timers_lua_thinker"})
-  GameRules:GetGameModeEntity():SetThink("think", self, THINK_NAME, THINK_INTERVAL)
+
+  GameRules:GetGameModeEntity():SetThink("think", self, THINK_NAME, self.interval)
 end
 
 function M:think()
@@ -74,7 +74,7 @@ function M:think()
     self:processTimer(timer)
   end
 
-  return THINK_INTERVAL
+  return self.interval
 end
 
 function M:processTimer(timer)
@@ -97,7 +97,7 @@ function M:processTimer(timer)
   end
 
   if removeTimer then
-    self.timers[timer.name] = nil
+    self.timers[timer.id] = nil
   end
 end
 
@@ -105,105 +105,147 @@ end
   Creates a timer with the given options.
 
   @usage
-    -- immediately calls `obj.fn(obj, timer)` (equivalent to `obj:fn(timer)`),
+    -- Immediately calls `obj.fn(obj, 13)` (equivalent to `obj:fn(13)`),
     -- if `fn` returns a number, repeats at that interval
-    Create({callback=obj.fn, context=obj})
+    Create({callback = obj.fn, args = {obj, 13}})
 
     -- 10 seconds delayed, run once using game time (respect pauses)
     Create({
-      at=10,
-      callback=function()
+      delay = 10.0,
+      callback = function()
         print("Hello. I'm running 10 seconds after when I was started.")
-      end,
+      end
     })
 
     -- 10 second delayed, run once regardless of pauses
     Create({
-      at=10,
-      realtime=true,
-      callback=function()
+      delay = 10.0,
+      realtime = true,
+      callback = function()
         print("Hello. I'm running 10 seconds after I was started even if someone paused the game.")
-      end,
+      end
+    })
+
+    -- At a specific time, run once using game time (respect pauses)
+    Create({
+      at = GameRules:GetGameTime() + 10.0,
+      callback = function()
+        print("Hello. I'm running when I was scheduled to run.")
+      end
+    })
+
+    -- At a specific time, run once using real time (regardless of pauses)
+    Create({
+      at = Time() + 10.0,
+      realtime = true,
+      callback = function()
+        print("Hello. I'm running when I was scheduled to run, even if someone paused the game.")
+      end
+    })
+
+    -- At a specific time, 10 second delay from that time, run once using game time (respect pauses)
+    Create({
+      at = GameRules:GetGameTime() + 30.0,
+      delay = 10.0,
+      callback = function()
+        print("Hello. I'm running 10 seconds after when I was scheduled to run.")
+      end
     })
 
     -- A timer running every second that starts 5 seconds in the future.
     -- Uses game time, respecting pauses.
     Create({
-      delay=5.0,
-      callback=function()
+      delay = 5.0,
+      callback = function()
         print("Hello. I'm running 5 seconds after you called me and then every second thereafter.")
         return 1.0
-      end,
+      end
     })
 
     -- A timer running every second that starts after 2 minutes regardless of pauses
     Create({
-      name="uniqueTimerString3",
-      delay=120,
-      realtime=true,
-      callback=function()
+      id = "uniqueTimerId",
+      delay = 120.0,
+      realtime = true,
+      callback = function()
         print("Hello. I'm running after 2 minutes and then every second thereafter.")
-        return 1
-      end,
+        return 1.0
+      end
     })
 
   @tparam table options Options table
   @tparam function options.callback Timer function
-  @tparam[opt] table options.context Context table to be passed to the callback
-  @tparam[opt=0] number|nil options.delay Delay start time (seconds).
-    `nil`, `0` or negative number means it starts immediately
+  @tparam[opt] string options.id Timer id
+  @tparam[opt] list options.args List of arguments to pass to the callback
+  @tparam[opt] number options.at Specific start time (timestamp in seconds).
+  `nil`, `0` or negative number means it starts immediately
+  @tparam[opt] number options.delay Delay start time (in seconds).
+  `nil`, `0` or negative number means it starts immediately
   @tparam[opt=false] bool options.realtime If `true`, uses real clock (ignores pauses)
+  @tparam[opt=false] bool options.paused If `true`, the timer starts paused and must be manually
+  unpaused with @{Unpause}
   @tparam[opt=nil] function options.onError Error handler function. If `nil`, uses @{defaultErrorHandler}
+  @todo Protect `self.timers` from concurrency
 ]]
 function M:Create(options)
   local timer = Timer(options)
 
-  assert(
-    self.timers[timer.name] == nil,
-    string.format("Invalid timer %q: timer already registered", timer.name)
-  )
+  assert(self.timers[timer.id] == nil, ERRF_INVALID_TIMER_ALREADY_REGISTERED:format(timer.id))
 
-  self.timers[timer.name] = timer
+  self.timers[timer.id] = timer
 
-  return timer.name
+  return timer.id
 end
 
---[[
--- FIXME: deal with concurrency with Think()
-function M:Pause(name)
-  local timer = self.timers[name]
+function M:setPaused(id, value)
+  local timer = self.timers[id]
 
   if timer == nil then
     return nil
   end
 
-  timer.paused = true
-  return timer
+  timer.paused = value
+  return id
 end
-]]
---[[
--- FIXME: deal with concurrency with Think()
-function M:Cancel(name)
-  local timer = self.timers[name]
-  self.timers[name] = nil
-  return timer
+
+--- Pauses a timer.
+-- @tparam string id Timer id
+-- @treturn ?string Id of paused timer or `nil` if the id isn't registered
+-- @todo Protect `self.timers` from concurrency
+function M:Pause(id)
+  return self:setPaused(id, true)
 end
-]]
---[[
--- FIXME: deal with concurrency with Think()
-function M:CancelAll(removePersistent)
-  if removePersistent then
-    self.timers = {}
-    return self
+
+--- Unpauses a timer.
+-- @tparam string id Timer id
+-- @treturn ?string Id of paused timer or `nil` if the id isn't registered
+-- @todo Protect `self.timers` from concurrency
+function M:Unpause(id)
+  return self:setPaused(id, false)
+end
+
+--- Cancels a timer.
+-- @tparam string id Timer id
+-- @treturn ?string Id of canceled timer or `nil` if the id isn't registered
+-- @todo Protect `self.timers` from concurrency
+function M:Cancel(id)
+  local timer = self.timers[id]
+  self.timers[id] = nil
+  return timer and id
+end
+
+--- Cancels all timers.
+-- @treturn array(string) List of timer ids that were canceled
+-- @todo Protect `self.timers` from concurrency
+function M:CancelAll()
+  local canceled = {}
+
+  for id, _ in pairs(self.timers) do
+    self.timers[id] = nil
+    table.insert(canceled, id)
   end
 
-  for _, timer in pairs(self.timers) do
-    if not timer.persist then
-      self.timers[timer.name] = nil
-    end
-  end
-
-  return self
+  return canceled
 end
-]]
+
 return M
