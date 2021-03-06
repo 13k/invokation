42ff9a156e6a1cdc22355a32013ffb8a316dc7b1
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2021-03-06 08:52:15 -0300

    Bump version to 0.4.4-beta1

--- content/panorama/scripts/lib/const.js
+++ content/panorama/scripts/lib/const.js
@@ -2,7 +2,7 @@
 
 (function (global /*, context */) {
   var META = {
-    VERSION: "v0.4.3-beta1",
+    VERSION: "v0.4.4-beta1",
     URL: "https://github.com/13k/invokation",
     CHANGELOG_URL: "https://github.com/13k/invokation/blob/master/CHANGELOG.md",
   };
