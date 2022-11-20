cccbbe0c8fa0148016a3938c3bcf65c85c80a9fd
13k <kbmurata@gmail.com>
2022-11-20 11:52:38 -0300

    chore: upgrade js deps, update yarn sdks, update eslint config to use typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
+};
