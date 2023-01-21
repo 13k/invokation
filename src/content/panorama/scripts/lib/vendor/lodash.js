fff7f7d59778c4be453d061bbc11d68120536d97
13k <kbmurata@gmail.com>
2023-01-21 00:40:51 -0300

    fix(panorama): patch vendored lodash to not use evaluated code

--- src/content/panorama/scripts/lib/vendor/lodash.js
+++ src/content/panorama/scripts/lib/vendor/lodash.js
@@ -433,7 +433,7 @@
   var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
 
   /** Used as a reference to the global object. */
-  var root = freeGlobal || freeSelf || Function('return this')();
+  var root = freeGlobal || freeSelf || this;
 
   /** Detect free variable `exports`. */
   var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
