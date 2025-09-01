bc79cb5a45780a862d078d05bbb01af4870dfb00
13k <k@13k.dev>
2025-09-01 19:34:42 -0300

    chore(lua): update `middleclass` types

--- .luacats/middleclass.lua
+++ .luacats/middleclass.lua
@@ -6,22 +6,28 @@
 local M = {}
 
 --- @class middleclass.Class
+--- @field name string
 --- @field static table
+--- @field class middleclass.Class
 local Class = {}
 
+--- @param class middleclass.Class
 --- @param ... any
 --- @return self
-function Class:new(...) end
-
---- @param mixin table
-function Class:include(mixin) end
+function Class.new(class, ...) end
 
 --- @param class middleclass.Class
---- @return boolean
-function Class:isInstanceOf(class) end
+--- @param mixin table
+function Class.include(class, mixin) end
 
+--- @param class middleclass.Class
 --- @param super middleclass.Class
 --- @return boolean
-function Class:isSubclassOf(super) end
+function Class.isSubclassOf(class, super) end
+
+--- @param self self
+--- @param class middleclass.Class
+--- @return boolean
+function Class.isInstanceOf(self, class) end
 
 return M
