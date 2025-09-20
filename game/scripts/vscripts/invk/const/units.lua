11b40946f5eb7cae6fd7c12559e700aec56052a1
13k <k@13k.dev>
2025-09-20 20:25:30 -0300

    feat: replace custom dummy target with hero demo dummy target

--- game/scripts/vscripts/invk/const/units.lua
+++ game/scripts/vscripts/invk/const/units.lua
@@ -6,7 +6,7 @@ local M = {}
 M.DUMMY_TARGET_SPAWN = "dummy_target_spawn"
 
 --- Dummy target unit name
-M.DUMMY_TARGET = "npc_invokation_dummy_target"
+M.DUMMY_TARGET = "npc_dota_hero_target_dummy"
 
 --- Invoker unit name
 M.INVOKER = "npc_dota_hero_invoker"
